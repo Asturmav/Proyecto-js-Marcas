dbe9b700f6ec1ff816be9e7d4d0baa0bb38833c4
Asturmav <dpro.royal@gmail.com>
2025-05-08 12:55:41 +0200

    Creados los archivos

---
+++ script.js
@@ -0,0 +1,14 @@
+function excusa(){
+    const sujetos = ["Mi perro", "El gato", "Mi abuela", "Un extraterrestre", "Mi hermano menor", 
+        "Un fantasma", "El vecino", "Mi robot doméstico", "Una cabra en patineta", "Un duende borracho", "Un dios Griego", "Un payaso interdimensional", 
+        "Mi profesor zombie", "Dora la Exploradora"]
+
+    const acciones = ["se comió", "tiró", "rompió", "escondió", "quemó", "mojó", "perdió", "hackeó", "metio", "secuestró", 
+        "vomitó sobre", "teletransportó", "sacrificó", "transformó en gelatina", "lanzó por la ventana", "convirtió en origami", 
+        "escribió encima con salsa", "tiró al volcán", "confundió con comida",  "invocó demonios con" ]
+
+    const objetos = ["mis deberes", "mi PC", "la tarea", "el informe", "mi movil", "el cuaderno", "el proyecto", "mi coche", "mi agujero negro de mascota", "mis dramas indios"]
+
+    const resultado = 
+    document.getElementById("excusa").innerHTML = "<strong>"+  + "</strong>";
+}
